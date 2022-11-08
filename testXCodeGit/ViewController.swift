15164b368120e8d8d3444f6746275bac2e6bc2b9
Omar_Elsaeed <omarelsaeed3@gmail.com>
2022-11-08 21:27:44 +0200

    Initial Commit

---
+++ testXCodeGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testXCodeGit
+//
+//  Created by Omar-Mac on 08/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
